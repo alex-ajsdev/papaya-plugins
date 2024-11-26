46c743e690f833d68efd4a60786345f148622ba8
alex-designlogica <148463732+alex-designlogica@users.noreply.github.com>
2024-11-26 13:29:33 +0000

    Added more automation to whisperer and region id to debugger

--- autowhisperer/src/main/java/net/runelite/client/plugins/autowhisperer/AutoWhispererPlugin.java
+++ autowhisperer/src/main/java/net/runelite/client/plugins/autowhisperer/AutoWhispererPlugin.java
@@ -1,60 +1,18 @@
 package net.runelite.client.plugins.autowhisperer;
 
-//2024-11-25 15:30:05 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - ==== Projectile Spawned ====
-//        2024-11-25 15:30:05 [Client] DEBUG n.r.c.p.debugger.DebuggerPlugin - Projectile ID: 2444, X: 0.0, Y: 0.0, Speed: 0.0, Height: 0.0, Interacting Name: GIM Papaya. Class Name: class Model
-//2024-11-25 15:30:07 [Client] DEBUG injected-client - Chat message type GAMEMESSAGE: <img=41><col=FF0000><shad><img=90>[Donator] </col><col=FF0000></col><shad><col=FF0000>Nl Mickey:</col> <col=FF0000>gim storage not working</col>
-//        2024-11-25 15:30:07 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - ==== Projectile Spawned ====
-//        2024-11-25 15:30:07 [Client] DEBUG n.r.c.p.debugger.DebuggerPlugin - Projectile ID: 2445, X: 0.0, Y: 0.0, Speed: 0.0, Height: 0.0, Interacting Name: GIM Papaya. Class Name: class Model
-//2024-11-25 15:30:08 [Client] DEBUG injected-client - Chat message type GAMEMESSAGE: <col=0040ff><shad><img=98>[Super Donator] </col><col=0040ff></col><shad><col=0040ff>Rng 404:</col> <col=0040ff>Mr. Moneybags over here.</col>
-//        2024-11-25 15:30:10 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - ==== Projectile Spawned ====
-//        2024-11-25 15:30:10 [Client] DEBUG n.r.c.p.debugger.DebuggerPlugin - Projectile ID: 2445, X: 0.0, Y: 0.
-//
-//2024-11-25 15:33:41 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - ==== Menu Option Clicked ====
-//        2024-11-25 15:33:41 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Menu Option: Prayer
-//2024-11-25 15:33:41 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Menu Target:
-//        2024-11-25 15:33:41 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Id: 1 ItemId: -1
-//        2024-11-25 15:33:41 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Menu Action: CC_OP
-//2024-11-25 15:33:41 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Param 0: -1
-//        2024-11-25 15:33:41 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Param 1: 10551359
-//        2024-11-25 15:33:41 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - ========================
-//        2024-11-25 15:33:42 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - ==== Menu Option Clicked ====
-//        2024-11-25 15:33:42 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Menu Option: Magic
-//2024-11-25 15:33:42 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Menu Target:
-//        2024-11-25 15:33:42 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Id: 1 ItemId: -1
-//        2024-11-25 15:33:42 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Menu Action: CC_OP
-//2024-11-25 15:33:42 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Param 0: -1
-//        2024-11-25 15:33:42 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Param 1: 10551360
-//        2024-11-25 15:33:42 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - ========================
-//2024-11-25 15:34:40 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - ========================
-//        2024-11-25 15:34:41 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - ==== Menu Option Clicked ====
-//        2024-11-25 15:34:41 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Menu Option: Activate
-//2024-11-25 15:34:41 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Menu Target: <col=ff9040>Protect from Magic</col>
-//        2024-11-25 15:34:41 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Id: 1 ItemId: -1
-//        2024-11-25 15:34:41 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Menu Action: CC_OP
-//2024-11-25 15:34:41 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Param 0: -1
-//        2024-11-25 15:34:41 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Param 1: 35454993
-//        2024-11-25 15:34:41 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - ========================
-//        2024-11-25 15:34:42 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - ==== Menu Option Clicked ====
-//        2024-11-25 15:34:42 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Menu Option: Activate
-//2024-11-25 15:34:42 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Menu Target: <col=ff9040>Protect from Missiles</col>
-//        2024-11-25 15:34:42 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Id: 1 ItemId: -1
-//        2024-11-25 15:34:42 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Menu Action: CC_OP
-//2024-11-25 15:34:42 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Param 0: -1
-//        2024-11-25 15:34:42 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - Param 1: 35454994
-//        2024-11-25 15:34:42 [Client] INFO  n.r.c.p.debugger.DebuggerPlugin - ========================
-
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
-import net.runelite.api.events.ProjectileSpawned;
+import net.runelite.api.events.*;
 import net.runelite.api.widgets.Widget;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+
 import org.pf4j.Extension;
 
 import javax.inject.Inject;
-import java.util.Map;
+import java.util.*;
 
 @Extension
 @PluginDescriptor(
@@ -72,27 +30,77 @@ public class AutoWhispererPlugin extends Plugin {
     @Inject
     private ClientThread clientThread;
 
+    private final List<TileItem> loot = new ArrayList<>();
+    private final Set<String> lootBlacklist = Set.of("manta ray", "super combat", "super attack", "bone");
+
     private final Map<Integer, Prayer> PRAYER_MAP = Map.of(
             2445, Prayer.PROTECT_FROM_MAGIC,
             2444, Prayer.PROTECT_FROM_MISSILES
     );
 
+    private static final int HOME_REGION = 12342;
+    private boolean pendingLootClear = false;
+    private static final int LOOT_CLEAR_DELAY = 10;
+    private int timeout = 0;
+    private boolean waitingForLoot = false;
+    private boolean teleportInProgress = false;
+
+
     @Subscribe
-    void onProjectileSpawned(ProjectileSpawned event) {
+    private void onItemSpawned(ItemSpawned event) {
+        int currentRegion = getCurrentRegion();
+        if (currentRegion != HOME_REGION) {
+            if (!isItemBlacklisted(event.getItem())) {
+                loot.add(event.getItem());
+            }
+        }
+    }
+
+    @Subscribe
+    private void onItemDespawned(ItemDespawned event) {
+        int currentRegion = getCurrentRegion();
+        if (currentRegion != HOME_REGION) {
+            loot.remove(event.getItem());
+        }
+    }
+
+    private boolean isItemBlacklisted(TileItem item) {
+        String itemName = client.getItemDefinition(item.getId()).getName();
+        return lootBlacklist.stream().anyMatch((blItem) -> itemName.toLowerCase().contains(blItem.toLowerCase()));
+    }
+
+    @Subscribe
+    private void onPlayerDespawned(PlayerDespawned event) {
+        Player player = client.getLocalPlayer();
+
+        if (event.getPlayer() == player) {
+            if (teleportInProgress) {
+                log.info("Player despawned due to teleport.");
+                teleportInProgress = false;
+                return;
+            }
+
+            log.info("Player despawned (likely died). Clearing loot list in {} ticks.", LOOT_CLEAR_DELAY);
+            pendingLootClear = true;
+            timeout = LOOT_CLEAR_DELAY;
+        }
+    }
+
+
+    @Subscribe
+    private void onProjectileSpawned(ProjectileSpawned event) {
         Projectile projectile = event.getProjectile();
         Prayer prayer = PRAYER_MAP.get(projectile.getId());
         if (prayer != null) {
             if (!client.isPrayerActive(prayer)) {
                 activatePrayer(prayer);
-            } else {
-                log.info("Prayer {} is already active. No action taken.", prayer.name());
             }
         }
     }
 
     private void activatePrayer(Prayer prayer) {
         Widget prayerWidget = client.getWidget(prayer.getWidgetInfo().getId());
-        if(prayerWidget == null) {
+        if (prayerWidget == null) {
             log.error("Could not find prayer widget");
             return;
         }
@@ -106,4 +114,182 @@ public class AutoWhispererPlugin extends Plugin {
                 prayerWidget.getId()
         );
     }
+
+
+    @Subscribe
+    private void onGameTick(GameTick event) {
+        if (timeout > 0) {
+            timeout--;
+            return;
+        }
+        if (pendingLootClear) {
+            log.info("Clearing loot list after death.");
+            loot.clear();
+            pendingLootClear = false;
+            return;
+        }
+
+        int currentRegion = getCurrentRegion();
+        if (currentRegion == HOME_REGION) {
+            log.info("Player is at home. Casting previous teleport...");
+            previousTeleport();
+            timeout = 2;
+        } else {
+            if (isWhispererAlive() && !isPlayerAttacking()) {
+                log.info("Whisperer is alive and not attacking. Attacking...");
+                attackWhisperer();
+                timeout = 2;
+            } else if (!isWhispererAlive()) {
+                if (loot.isEmpty() && !waitingForLoot) {
+                    log.info("Whisperer is dead. Waiting for loot to spawn...");
+                    timeout = 4;
+                    waitingForLoot = true;
+                } else if (!loot.isEmpty()) {
+                    log.info("Whisperer is dead. Looting items...");
+                    lootItems();
+                    timeout = 1;
+                    waitingForLoot = false;
+                } else if (isLowOnPrayer() && loot.isEmpty()) {
+                    log.info("Low on prayer. Casting previous teleport...");
+                    homeTeleport();
+                    timeout = 10;
+                } else {
+                    log.info("Whisperer is dead, no loot available. Starting new instance...");
+                    previousTeleport();
+                    timeout = 2;
+                    waitingForLoot = false;
+                }
+            }
+        }
+    }
+
+    private int getCurrentRegion() {
+        return client.getLocalPlayer().getWorldLocation().getRegionID();
+    }
+
+    private boolean isLowOnPrayer() {
+        int currentPrayer = client.getBoostedSkillLevel(Skill.PRAYER);
+        int maxPrayer = client.getRealSkillLevel(Skill.PRAYER);
+        return currentPrayer < maxPrayer * 0.2; // Less than 20% of max prayer
+    }
+
+    private boolean isWhispererAlive() {
+        return client.getNpcs().stream()
+                .anyMatch(npc -> npc.getName() != null && npc.getName().toLowerCase().contains("whisperer"));
+    }
+
+    private boolean isPlayerAttacking() {
+        Actor interacting = client.getLocalPlayer().getInteracting();
+        if (interacting == null) {
+            return false;
+        }
+
+        if (!(interacting instanceof NPC)) {
+            return false;
+        }
+        NPC npc = (NPC) interacting;
+
+        return npc.getName() != null && npc.getName().toLowerCase().contains("whisperer");
+    }
+
+    private void attackWhisperer() {
+        Optional<NPC> npcOptional = client.getNpcs().stream()
+                .filter(npc -> npc.getName() != null && npc.getName().toLowerCase().contains("whisperer"))
+                .findFirst();
+
+        if (npcOptional.isEmpty()) {
+            log.info("Could not find whisperer");
+            return;
+        }
+        NPC whisperer = npcOptional.get();
+        client.invokeMenuAction(
+                "Attack",
+                whisperer.getName(),
+                whisperer.getIndex(),
+                MenuAction.NPC_SECOND_OPTION.getId(),
+                0,
+                0
+        );
+    }
+
+
+    private void lootItems() {
+        if (loot.isEmpty()) {
+            log.info("No loot available to pick up.");
+            return;
+        }
+
+        log.info("Loot available:");
+        for (TileItem item : loot) {
+            String itemName = client.getItemDefinition(item.getId()).getName();
+            log.info("- {}", itemName);
+        }
+
+        // Pick up the nearest item
+        TileItem lootItem = getNearestTileItem(loot);
+        if (lootItem != null) {
+            client.invokeMenuAction(
+                    "Take",
+                    client.getItemDefinition(lootItem.getId()).getName(),
+                    lootItem.getId(),
+                    MenuAction.GROUND_ITEM_THIRD_OPTION.getId(),
+                    lootItem.getTile().getSceneLocation().getX(), lootItem.getTile().getSceneLocation().getY()
+            );
+        }
+    }
+
+    private TileItem getNearestTileItem(List<TileItem> tileItems) {
+        Player player = client.getLocalPlayer();
+
+        int currentDistance;
+        TileItem closestTileItem = tileItems.get(0);
+        int closestDistance = closestTileItem.getTile().getWorldLocation().distanceTo(player.getWorldLocation());
+        for (TileItem tileItem : tileItems) {
+            currentDistance = tileItem.getTile().getWorldLocation().distanceTo(player.getWorldLocation());
+            if (currentDistance < closestDistance) {
+                closestTileItem = tileItem;
+                closestDistance = currentDistance;
+            }
+        }
+        return closestTileItem;
+    }
+
+    private void homeTeleport() {
+        teleportInProgress = true;
+        client.invokeMenuAction(
+                "Cast",
+                "<col=00ff00>Edgeville Home Teleport</col>",
+                1,
+                MenuAction.CC_OP.getId(),
+                -1,
+                14286948
+        );
+    }
+
+    private void previousTeleport() {
+        client.invokeMenuAction(
+                "Previous-teleport",
+                "<col=00ff00>Edgeville Home Teleport</col>",
+                2,
+                MenuAction.CC_OP.getId(),
+                -1,
+                14286948
+        );
+        clientThread.invokeLater(this::widgetContinue);
+    }
+
+    private void widgetContinue() {
+        teleportInProgress = true;
+        client.invokeMenuAction(
+                "Continue",
+                "",
+                0,
+                MenuAction.WIDGET_CONTINUE.getId(),
+                1,
+                14352385
+        );
+    }
+
 }
+
+
--- debugger/src/main/java/net/runelite/client/plugins/debugger/DebuggerPlugin.java
+++ debugger/src/main/java/net/runelite/client/plugins/debugger/DebuggerPlugin.java
@@ -2,13 +2,17 @@ package net.runelite.client.plugins.debugger;
 
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Actor;
+import net.runelite.api.Client;
 import net.runelite.api.Projectile;
+import net.runelite.api.events.GameTick;
 import net.runelite.api.events.MenuOptionClicked;
 import net.runelite.api.events.ProjectileSpawned;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import org.pf4j.Extension;
+
+import javax.inject.Inject;
 
 
 @Extension
@@ -20,7 +24,9 @@ import org.pf4j.Extension;
 )
 @Slf4j
 public class DebuggerPlugin extends Plugin {
-
+    @Inject
+    private Client client;
+    private int tick = 0;
     @Subscribe
     void onMenuOptionClicked(MenuOptionClicked event) {
         log.info("==== Menu Option Clicked ====");
@@ -42,7 +48,14 @@ public class DebuggerPlugin extends Plugin {
         log.debug("Projectile ID: {}, X: {}, Y: {}, Speed: {}, Height: {}, Interacting Name: {}. Class Name: {}",
                 projectile.getId(), projectile.getX(), projectile.getY(),
                 projectile.getVelocityX(), projectile.getVelocityY(), interactingName, className);
+    }
 
-
+    @Subscribe
+    void onGameTick(GameTick event) {
+        tick++;
+        if(tick >= 10) {
+            log.info("Region ID: {}", client.getLocalPlayer().getWorldLocation().getRegionID() );
+            tick = 0;
+        }
     }
 }
